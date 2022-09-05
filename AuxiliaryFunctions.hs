289f5ec779247d6dbab7004b51d99f84bf2b635f
Łukasz Abramowicz <64956485+ghostbuster265@users.noreply.github.com>
2022-09-05 02:08:19 +0200

    Final update

--- AuxiliaryFunctions.hs
+++ AuxiliaryFunctions.hs
@@ -1,19 +1,22 @@
 module AuxiliaryFunctions where
 
------------
+import Data.List
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) | x `elem` xs   = rmdups xs
+              | otherwise     = x : rmdups xs
 
 subsets :: Eq a => [a] -> [[a]]
 subsets [] = [[]]
 subsets (x:xs) = yss ++ map (x:) yss
     where yss = subsets xs
 
-seplegal :: Eq a => [a] -> [a] -> Bool
-seplegal [] _ = True
-seplegal _ [] = True
-seplegal xs ys = not (elem xs (subsets ys)) && not (elem ys (subsets xs))
+seplegal :: Ord a => [a] -> [a] -> Bool
+seplegal xs ys = sort xs == sort ys
 
-contextsep :: Eq a => [a] -> [([a],[a])]
-contextsep xs = [(f,s) | f <- subsets xs, s <- subsets xs, length f + length s == length xs, seplegal f s]
+contextsep :: Ord a => [a] -> [([a],[a])]
+contextsep xs = rmdups [(f,s) | f <- subsets xs, s <- subsets xs, seplegal (f ++ s) xs]
 
 pick3 :: Eq a => [a] -> [[a]]
 pick3 xs = [s | s <- subsets xs, length s == 3]
