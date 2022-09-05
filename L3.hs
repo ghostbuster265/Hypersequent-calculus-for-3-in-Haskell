7e356f38a5d159e6196cd7753c77772fadcc6918
Łukasz Abramowicz <64956485+ghostbuster265@users.noreply.github.com>
2022-09-05 02:04:34 +0200

    Final update

--- L3.hs
+++ L3.hs
@@ -1,7 +1,6 @@
 module L3 where
 
 import Language
---import LanguageMbC
 import Data.Tree
 import Data.List
 import AuxiliaryFunctions
@@ -10,7 +9,6 @@ import AuxiliaryFormulas
 l3_orderseq :: L3Seq -> L3Seq
 l3_orderseq (L3S (L3 (xs, ys, zs, is), (L3 (xs', ys', zs', is')))) = L3S (l3_insertcontextL (xs ++ ys ++ zs ++ is) (L3 ([],[],[],[])), l3_insertcontextR (xs' ++ ys' ++ zs' ++ is') (L3 ([],[],[],[])))
 
--- | Insert a singular formula in a proper list in a sequent, depending on its form seperately for the right and left side of a sequent (functions l3_insertL and l3_insertR)
 l3_insertL :: For -> L3SeqSide -> L3SeqSide
 l3_insertL f (L3 (xs, ys, zs, is))
 	| var f            = L3 (f:xs, ys, zs, is)
@@ -23,7 +21,7 @@ l3_insertR f (L3 (xs, ys, zs, is))
 	| var f            = L3 (f:xs, ys, zs, is)
 	| nonbranchingR f  = L3 (xs, f:ys, zs, is)
 	| branchingR f     = L3 (xs, ys, f:zs, is)
-
+	
 l3_insertcontextL :: [For] -> L3SeqSide -> L3SeqSide
 l3_insertcontextL [] s = s
 l3_insertcontextL (f:fs) s = l3_insertcontextL fs (l3_insertL f s)
@@ -31,8 +29,7 @@ l3_insertcontextL (f:fs) s = l3_insertcontextL fs (l3_insertL f s)
 l3_insertcontextR :: [For] -> L3SeqSide -> L3SeqSide
 l3_insertcontextR [] s = s
 l3_insertcontextR (f:fs) s = l3_insertcontextR fs (l3_insertR f s)
-
--- | Generating all the possible separation of  
+ 
 l3_sepcontexts :: [L3Seq] -> [L3Seq] -> [(([L3Seq],[L3Seq]),([For],[For]),([For],[For]))]
 l3_sepcontexts (x:xs) g
 	| l3_atomicseq x = l3_sepcontexts xs g
@@ -41,10 +38,10 @@ l3_sepcontexts (x:xs) g
 l3_sepcontexts [] _ = []
 
 l3_mixingruleHypL :: [L3Seq] -> [For] -> [For] -> [L3Seq] -> [L3Seq]
-l3_mixingruleHypL (x:xs) ls rs (m1:m2:m3:[]) = [L3S (l3_insertcontextL ls (L3 ([],[],[],[])), l3_insertcontextR rs (L3 ([],[],[],[])))] ++ (delete m3 (delete m2 (delete m1 xs)))
+l3_mixingruleHypL xs ls rs (m1:m2:m3:[]) = [L3S (l3_insertcontextL ls (L3 ([],[],[],[])), l3_insertcontextR rs (L3 ([],[],[],[])))] ++ (delete m3 (delete m2 (delete m1 xs)))
 
 l3_mixingruleHypR :: [L3Seq] -> [For] -> [For] -> [L3Seq] -> [L3Seq]
-l3_mixingruleHypR (x:xs) ls rs (m1:m2:m3:[]) = [L3S (l3_insertcontextL ls (L3 ([],[],[],[])), l3_insertcontextR rs (L3 ([],[],[],[])))] ++ (delete m3 (delete m2 (delete m1 xs)))
+l3_mixingruleHypR xs ls rs (m1:m2:m3:[]) = [L3S (l3_insertcontextL ls (L3 ([],[],[],[])), l3_insertcontextR rs (L3 ([],[],[],[])))] ++ (delete m3 (delete m2 (delete m1 xs)))
 
 l3_contextruleHypR :: [L3Seq] -> [L3Seq] -> [For] -> [For] -> [L3Seq]
 l3_contextruleHypR (x:xs) hs ls rs
@@ -76,7 +73,6 @@ l3_branchingruleHypL (x:xs)
 		L3S (l, L3 (xs',ys, A m _ :zs,is)) -> [L3S (l, l3_insertR m (L3 (xs',ys,zs,is)))] ++ xs
 l3_branchingruleHypL [] = []
 
--- | Applying function l3_nonbranchingruleSeq on a given hypersequent.*
 l3_nonbranchingruleHyp :: [L3Seq] -> [L3Seq]
 l3_nonbranchingruleHyp (x:xs) 
 	| l3_branchingseq x || l3_atomicseq x || l3_contextseq x = (x:l3_nonbranchingruleHyp xs)
@@ -90,28 +86,26 @@ l3_nonbranchingruleHyp [] = []
 
 l3_triplebranchingR :: [L3Seq] -> [L3Seq]
 l3_triplebranchingR (x:xs) = case x of
-	L3S (L3 (x,y,z, I3 _ n:is), r) -> [L3S (l3_insertL n (L3 (x,y,z,is)), r)]
-	_ -> l3_triplebranchingR (xs)
+    L3S (L3 (x,y,z, I3 _ n:is), r) -> [L3S (l3_insertL n (L3 (x,y,z,is)), r)]
+    _ -> l3_triplebranchingR (xs)
 l3_triplebranchingR [] = []
 
 l3_triplebranchingM :: ([L3Seq],[L3Seq]) -> [L3Seq]
 l3_triplebranchingM ((x:xs),(l':m':r':[])) = case x of
-	L3S (L3 (x,y,z, I3 m n:is), r) -> [L3S (l3_insertL n (L3 ([],[],[],[])), L3 ([],[],[],[])),m',L3S (L3 ([],[],[],[]), l3_insertR m (L3 ([],[],[],[])))] ++ (delete r' (delete m' (delete r' xs)))
-	_ -> l3_triplebranchingM (xs,(l':m':r':[]))
+    L3S (L3 (x,y,z, I3 m n:is), r) -> [L3S (l3_insertL n (L3 ([],[],[],[])), L3([],[],[],[])),m',L3S (L3 ([],[],[],[]), l3_insertR m (L3 ([],[],[],[])))] ++ (delete r' (delete m' (delete r' xs)))
+    _ -> l3_triplebranchingM (xs,(l':m':r':[]))
 l3_triplebranchingM ([],_) = []
 
 l3_triplebranchingL :: [L3Seq] -> [L3Seq]
 l3_triplebranchingL (x:xs) = case x of
-	L3S (L3 (x,y,z, I3 m _:is), r) -> [L3S (L3 (x,y,z,is), l3_insertR m r)]
-	_ -> l3_triplebranchingL (xs)
+    L3S (L3 (x,y,z, I3 m _:is), r) -> [L3S (L3 (x,y,z,is), l3_insertR m r)]
+    _ -> l3_triplebranchingL (xs)
 l3_triplebranchingL [] = []
 ----------------------------------------------------------------------------------------------------------------------------------------
-	
--- | Checks if a given sequent is an atomic one, that is, there are only variables left and nothing left to resolve.
-l3_contextseq :: L3Seq -> Bool
-l3_contextseq (L3S (L3 (x, y, z, i),L3 (x', y', z', _))) = if null (y ++ z ++ y' ++ z') && not (null i) then True else False
 
--- | Checks if a given sequent is an atomic one, that is, there are only variables left and nothing left to resolve.
+l3_contextseq :: L3Seq -> Bool
+l3_contextseq (L3S (L3 (_, y, z, i),L3 (_, y', z', _))) = if null (y ++ z ++ y' ++ z') && not (null i) then True else False
+
 l3_atomicseq :: L3Seq -> Bool
 l3_atomicseq (L3S (L3 (_, y, z, i),L3 (_, y', z', _))) = if null (y ++ z ++ i ++ y' ++ z') then True else False
 
@@ -122,28 +116,27 @@ l3_branchingseq (L3S (L3 (_, ys, _, _),L3 (_, ys', _, _))) = if null (ys ++ ys')
 l3_branching :: [L3Seq] -> Bool
 l3_branching xs = all l3_branchingseq xs
 
--- | Builds a tree for an argument of a tree type.
 l3_buildcontextTree :: LT -> LT
 l3_buildcontextTree (Leaf3 x)
-    	| and (map (\s -> l3_atomicseq s || l3_contextseq s) x) = (Leaf3 x)
+        | and (map (\s -> l3_atomicseq s || l3_contextseq s) x) = (Leaf3 x)
         | not (l3_branching x) = (Node3 x (Leaf3 (l3_nonbranchingruleHyp x)))
-		| otherwise = (BranchingNode3 x (Leaf3 (l3_branchingruleHypL x)) (Leaf3 (l3_branchingruleHypR x)))
+        | otherwise = (BranchingNode3 x (Leaf3 (l3_branchingruleHypL x)) (Leaf3 (l3_branchingruleHypR x)))
 l3_buildcontextTree (Node3 x ch) = Node3 x (l3_buildcontextTree ch)
 l3_buildcontextTree (BranchingNode3 x l r) = BranchingNode3 x (l3_buildcontextTree l) (l3_buildcontextTree r)
 l3_buildcontextTree (TrippleNode3 x l m r) = TrippleNode3 x (l3_buildcontextTree l) (l3_buildcontextTree m) (l3_buildcontextTree r)
 
 l3_insertContext :: LT -> (([L3Seq],[L3Seq]),([For],[For]),([For],[For])) -> LT
 l3_insertContext (Leaf3 x) (h,l,r)
-    	| all l3_atomicseq x = (Leaf3 x)
-		| otherwise = (BranchingNode3 x (Leaf3 (l3_contextruleHypL x (fst h) (fst l) (fst r))) (Leaf3 (l3_contextruleHypR x (snd h) (snd l) (snd r))))
+        | all l3_atomicseq x = (Leaf3 x)
+        | otherwise = (BranchingNode3 x (Leaf3 (l3_contextruleHypL x (fst h) (fst l) (fst r))) (Leaf3 (l3_contextruleHypR x (snd h) (snd l) (snd r))))
 l3_insertContext (Node3 x ch) c = Node3 x (l3_insertContext ch c)
 l3_insertContext (BranchingNode3 x l r) c
-	| not (l3_atomTree l) = BranchingNode3 x (l3_insertContext l c) r
-	| otherwise = BranchingNode3 x l (l3_insertContext r c)
+        | not (l3_atomTree l) = BranchingNode3 x (l3_insertContext l c) r
+        | otherwise = BranchingNode3 x l (l3_insertContext r c)
 l3_insertContext (TrippleNode3 x l m r) c
-	| not (l3_atomTree l) = TrippleNode3 x (l3_insertContext l c) m r
-	| not (l3_atomTree m) = TrippleNode3 x l (l3_insertContext m c) r
-	| otherwise = TrippleNode3 x l m (l3_insertContext r c)
+        | not (l3_atomTree l) = TrippleNode3 x (l3_insertContext l c) m r
+        | not (l3_atomTree m) = TrippleNode3 x l (l3_insertContext m c) r
+        | otherwise = TrippleNode3 x l m (l3_insertContext r c)
 
 l3_posscontexts :: LT -> [(([L3Seq],[L3Seq]),([For],[For]),([For],[For]))]
 l3_posscontexts (Leaf3 x) = l3_sepcontexts x x
@@ -184,30 +177,31 @@ l3_tripleTree (TrippleNode3 x l m r)
 
 l3_tripleseq :: [L3Seq] -> [L3Seq] -> ([L3Seq],[L3Seq])
 l3_tripleseq (x:xs) h = case x of
-	L3S (L3 (x',y,z, I3 m n:is), r) -> ([L3S (L3 (x',y,z, I3 m n:is), r),L3S (L3 (x',y,z,is), r),L3S (L3 (x',y,z, I3 m n:is), r)] ++ (delete (L3S (L3 (x',y,z, I3 m n:is), r)) h),[L3S (L3 (x',y,z, I3 m n:is), r),L3S (L3 (x',y,z,is), r),L3S (L3 (x',y,z, I3 m n:is), r)])
-	_ -> l3_tripleseq xs h
+    L3S (L3 (x',y,z, I3 m n:is), r) -> ([L3S (L3 (x',y,z, I3 m n:is), r),L3S (L3 (x',y,z,is), r),L3S (L3 (x',y,z, I3 m n:is), r)] ++ (delete (L3S (L3 (x',y,z, I3 m n:is), r)) h),[L3S (L3 (x',y,z, I3 m n:is), r),L3S (L3 (x',y,z,is), r),L3S (L3 (x',y,z, I3 m n:is), r)])
+    _ -> l3_tripleseq xs h
 
 l3_mixcontexts :: [L3Seq] -> [(([For],[For]),([For],[For]))]
 l3_mixcontexts [L3S (L3 (xs1,ys1,zs1,is1), L3 (xs1',ys1',zs1',is1')),L3S (L3 (xs2,ys2,zs2,is2), L3 (xs2',ys2',zs2',is2')),L3S (L3 (xs3,ys3,zs3,is3), L3 (xs3',ys3',zs3',is3'))] =
-	[((ll1 ++ ll2 ++ ll3, lr1 ++ lr2 ++ lr3), (rl1 ++ rl2 ++ rl3, rr1 ++ rr2 ++ rr3)) |
-		ll1 <- map (fst) s1, ll2 <- map (fst) s2, ll3 <- map (fst) s3,
-		lr1 <- map (snd) s1, lr2 <- map (snd) s2, lr3 <- map (snd) s3,
-		rl1 <- map (fst) s1', rl2 <- map (fst) s2', rl3 <- map (fst) s3',
-		rr1 <- map (snd) s1', rr2 <- map (snd) s2', rr3 <- map (snd) s3',
-		seplegal (ll1 ++ ll2 ++ ll3)  (lr1 ++ lr2 ++ lr3), seplegal (rl1 ++ rl2 ++ rl3) (rr1 ++ rr2 ++ rr3)]
-		where (s1,s2,s3,s1',s2',s3') = ((contextsep (xs1 ++ ys1 ++ zs1 ++ is1)), (contextsep (xs2 ++ ys2 ++ zs2 ++ is2)), (contextsep (xs3 ++ ys3 ++ zs3 ++ is3)),
-			  (contextsep (xs1' ++ ys1' ++ zs1' ++ is1')), (contextsep (xs2' ++ ys2' ++ zs2' ++ is2')), (contextsep (xs3'++ ys3' ++ zs3' ++ is3')))
+	rmdups [((ll1 ++ ll2 ++ ll3, lr1 ++ lr2 ++ lr3), (rl1 ++ rl2 ++ rl3, rr1 ++ rr2 ++ rr3)) |
+			ll1 <- map (fst) s1, ll2 <- map (fst) s2, ll3 <- map (fst) s3,
+			lr1 <- map (snd) s1, lr2 <- map (snd) s2, lr3 <- map (snd) s3,
+			rl1 <- map (fst) s1', rl2 <- map (fst) s2', rl3 <- map (fst) s3',
+			rr1 <- map (snd) s1', rr2 <- map (snd) s2', rr3 <- map (snd) s3',
+			seplegal ((ll1 ++ ll2 ++ ll3) ++ (lr1 ++ lr2 ++ lr3) ++ (rl1 ++ rl2 ++ rl3) ++ (rr1 ++ rr2 ++ rr3)) ((xs1 ++ ys1 ++ zs1 ++ is1) ++ (xs2 ++ ys2 ++ zs2 ++ is2)
+			++ (xs3 ++ ys3 ++ zs3 ++ is3) ++ (xs1' ++ ys1' ++ zs1' ++ is1') ++ (xs2' ++ ys2' ++ zs2' ++ is2') ++ (xs3'++ ys3' ++ zs3' ++ is3'))]
+			where (s1,s2,s3,s1',s2',s3') = ((contextsep (xs1 ++ ys1 ++ zs1 ++ is1)), (contextsep (xs2 ++ ys2 ++ zs2 ++ is2)), (contextsep (xs3 ++ ys3 ++ zs3 ++ is3)),
+				(contextsep (xs1' ++ ys1' ++ zs1' ++ is1')), (contextsep (xs2' ++ ys2' ++ zs2' ++ is2')), (contextsep (xs3'++ ys3' ++ zs3' ++ is3')))
 
 l3_pick3 :: LT -> [[L3Seq]]
 l3_pick3 (Leaf3 x) = pick3 x
 l3_pick3 (Node3 _ ch) = l3_pick3 ch
 l3_pick3 (BranchingNode3 _ l r)
-	| l3_mixTree l = l3_pick3 l
-	| otherwise = l3_pick3 r
+        | l3_mixTree l = l3_pick3 l
+        | otherwise = l3_pick3 r
 l3_pick3 (TrippleNode3 _ l m r)
-	| l3_mixTree l = l3_pick3 l
-	| l3_mixTree m = l3_pick3 m
-	| otherwise = l3_pick3 r 
+        | l3_mixTree l = l3_pick3 l
+        | l3_mixTree m = l3_pick3 m
+        | otherwise = l3_pick3 r 
 
 l3_mixedTrees :: LT -> [[L3Seq]] -> [LT]
 l3_mixedTrees t (x:xs) = [l3_insertMixed t c x | c <- l3_mixcontexts x] ++ (l3_mixedTrees t xs)
@@ -219,7 +213,6 @@ l3_mixTree (Node3 _ ch) = l3_mixTree ch
 l3_mixTree (BranchingNode3 _ l r) = (l3_mixTree l) || (l3_mixTree r)
 l3_mixTree (TrippleNode3 _ l m r) = (l3_mixTree l) || (l3_mixTree m) || (l3_mixTree r)
 
--- | Checks if all of the leaves in a tree are atomic, so there is nothing left to resolve.
 l3_contextTree :: LT -> Bool
 l3_contextTree (Leaf3 x) = if all l3_contextseq x then True else False
 l3_contextTree (Node3 _ ch) = l3_contextTree ch
@@ -235,19 +228,18 @@ l3_atomTree (TrippleNode3 _ l m r) = (l3_atomTree l) && (l3_atomTree m) && (l3_a
 l3_buildTrees :: [LT] -> [LT]
 l3_buildTrees [] = []
 l3_buildTrees (t:ts)
-	| l3_isProof t = t : (l3_buildTrees ts)
-	| not (l3_atomTree t) && not (l3_contextTree t) = (l3_buildcontextTree t) : ts
-	| l3_mixTree t = case () of
-		() | any l3_isProof (l3_derHyp (l3_mixedTrees t (l3_pick3 t))) -> (l3_mixedTrees t (l3_pick3 t)) ++ ts
-		   | l3_contextTree t -> if any l3_isProof (l3_derHyp (l3_altTrees t)) then (l3_altTrees t) ++ ts else [l3_tripleTree t] ++ ts
-		   | otherwise -> l3_buildTrees ts
-	| l3_contextTree t = if any l3_isProof (l3_derHyp (l3_altTrees t)) then (l3_altTrees t) ++ ts else [l3_tripleTree t] ++ ts
-	| otherwise = l3_buildTrees ts
+    | l3_isProof t = t : (l3_buildTrees ts)
+    | not (l3_atomTree t) && not (l3_contextTree t) = (l3_buildcontextTree t) : ts
+    | l3_mixTree t = case () of
+        () | not (null (l3_derHyp (l3_mixedTrees t (l3_pick3 t)))) -> (l3_mixedTrees t (l3_pick3 t)) ++ ts
+           | l3_contextTree t -> if null (l3_derHyp (l3_altTrees t)) then [l3_tripleTree t] ++ ts else (l3_altTrees t) ++ ts
+           | otherwise -> l3_buildTrees ts
+    | l3_contextTree t = if null (l3_derHyp (l3_altTrees t)) then [l3_tripleTree t] ++ ts else (l3_altTrees t) ++ ts
+    | otherwise = l3_buildTrees ts
 	
 l3_resolved :: [LT] -> Bool
 l3_resolved ts = all l3_isProof ts
 
--- | Builds all derivation for a hypersequent until there are only atoms left starting with an initial leaf that contains the given hypersequent.
 l3_derHyp :: [LT] -> [LT]
 l3_derHyp x = until (l3_resolved) l3_buildTrees x 
 
@@ -270,16 +262,14 @@ l3_levels (TrippleNode3 _ l m r) = 1 + (l3_levels l) + (l3_levels m) + (l3_level
 
 l3_matchingformulas :: [For] -> [For] -> Bool
 l3_matchingformulas [] ys = False
-l3_matchingformulas (x:xs) ys = if x `elem` ys then True else l3_matchingformulas xs ys
+l3_matchingformulas (x:xs) ys = x `elem` ys || l3_matchingformulas xs ys
 
 l3_isAxiom :: L3Seq -> Bool
 l3_isAxiom (L3S (L3 (xs, ys, zs, is), L3 (xs', ys', zs', is'))) = l3_matchingformulas l r || checkform l l || checkform r r
-	where (l,r) = ((xs ++ ys ++ zs ++ is),(xs' ++ ys' ++ zs' ++ is'))
+    where (l,r) = ((xs ++ ys ++ zs ++ is),(xs' ++ ys' ++ zs' ++ is'))
 
 l3_isProof :: LT -> Bool
-l3_isProof (Leaf3 x) = any (l3_isAxiom) x
+l3_isProof (Leaf3 x) = any l3_isAxiom x
 l3_isProof (Node3 _ ch) = l3_isProof ch
 l3_isProof (BranchingNode3 _ l r) = (l3_isProof l) && (l3_isProof r)
 l3_isProof (TrippleNode3 _ l m r) = (l3_isProof l) && (l3_isProof m) && (l3_isProof r)
-
------------------------------------------------------------------------------------------------
